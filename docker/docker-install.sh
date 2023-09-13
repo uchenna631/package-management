9cdcb6f24697d3c8efdb2685190a30c9eb81eeb3
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-09-13 22:27:03 +0100

    Update docker-install.sh

--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -4,10 +4,14 @@
 # Uninstall the Docker Engine, CLI, containerd, and Docker Compose packages:
 sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
 
-# Remove images,  containers, volun=mes or custom configurations
+# Remove images,  containers, volumes or custom configurations
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
 
-# Install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
+#!bin/bash
+sudo hostname docker
+sudo apt update -y
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo su - ubuntu
+
