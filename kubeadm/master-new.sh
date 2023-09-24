8433483dbafbbd8361810c306d02e2278124f148
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-09-24 05:56:52 +0100

    Update master-new.sh

--- kubeadm/master-new.sh
+++ kubeadm/master-new.sh
@@ -17,13 +17,32 @@ net.ipv4.ip_forward                 = 1
 EOF
 
 sudo sysctl --system
+#4 Install containerd run time
+# To install containerd, first install its dependencies
+
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt-get install -y apt-transport-https ca-certificates curl gnup lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
 sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 # This overwrites an existing configuration in /etc/apt/sources.list.d/kubernetes.list
 sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#Install containerd
+sudo apt-get update -y
+sudo apt-get install containerd.io -y
+
+#Generate default configuration file for container
+sudo containerd config default > /etc/containerd/config.toml
+
+# Run following command to update configure cgroup as systemd for contianerd.
+
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
+# Restart and enable containerd service
+
+systemctl restart containerd
+systemctl enable containerd
 
 sudo apt-get update -y
 sudo apt-get install -y kubelet kubeadm kubectl
