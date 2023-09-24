90f304f962434bfaf3551bef612176b00ffb4ea5
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-09-24 07:17:03 +0100

    Update master-new.sh

--- kubeadm/master-new.sh
+++ kubeadm/master-new.sh
@@ -10,7 +10,8 @@ br_netfilter
 EOF
 
 sudo modprobe overlay
-sudo modprobe br_netfiltersudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo modprobe br_netfilter
+sudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
