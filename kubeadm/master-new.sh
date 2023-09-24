0143efeaf6e0117e175cc52917778274c30d9a30
Fimber Uchenna Eze <uchenna631@gmail.com>
2023-09-24 04:33:17 +0100

    Update master-new.sh
    Installing k8s with bash script

--- kubeadm/master-new.sh
+++ kubeadm/master-new.sh
@@ -1,107 +1,33 @@
-#!/bin/bash
-#i1) Switch to root user [ sudo -i]
-
-sudo hostnamectl set-hostname  master
-
-#2) Disable swap & add kernel settings
-
-swapoff -a
-sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-
-
-#3) Add  kernel settings & Enable IP tables(CNI Prerequisites)
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+sudo apt-get update
+# set hostname. use the `nodeName` for a node
+sudo hostname master
+# apt-transport-https may be a dummy package; if so, you can skip that package
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
 
-modprobe overlay
-modprobe br_netfilter
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo modprobe overlay
+sudo modprobe br_netfiltersudo cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
 
-sysctl --system
+sudo sysctl --system
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-#4) Install containerd run time
-
-#To install containerd, first install its dependencies.
-
-apt-get update -y
-apt-get install ca-certificates curl gnupg lsb-release -y
-
-#Note: We are not installing Docker Here.Since containerd.io package is part of docker apt repositories hence we added docker repository & it's key to download and install containerd.
-# Add Docker’s official GPG key:
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# This overwrites an existing configuration in /etc/apt/sources.list.d/kubernetes.list
+sudo echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#Use follwing command to set up the repository:
-
-#echo \
-#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
-
-# Install containerd
-
-apt-get update -y
-apt-get install containerd.io -y
-
-# Generate default configuration file for containerd
-
-#Note: Containerd uses a configuration file located in /etc/containerd/config.toml for specifying daemon level options.
-#The default configuration can be generated via below command.
-
-containerd config default > /etc/containerd/config.toml
-
-# Run following command to update configure cgroup as systemd for contianerd.
-
-sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
-
-# Restart and enable containerd service
-
-systemctl restart containerd
-systemctl enable containerd
-
-#5) Installing kubeadm, kubelet and kubectl
-
-# Update the apt package index and install packages needed to use the Kubernetes apt repository:
-
-apt-get update
-apt-get install -y apt-transport-https ca-certificates curl
-
-# Download the Google Cloud public signing key:
-
-#curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-# Add the Kubernetes apt repository:
-
-#echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-# Update apt package index, install kubelet, kubeadm and kubectl, and pin their version:
-
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-
-# apt-mark hold will prevent the package from being automatically upgraded or removed.
-
-apt-mark hold kubelet kubeadm kubectl
-
-# Enable and start kubelet service
-
-systemctl daemon-reload
-systemctl start kubelet
-systemctl enable kubelet.service
-
-# initialise the control plane
-kubeadm init 
-
-su - ubuntu
-~
+sudo apt-get update -y
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl daemon-reload
+sudo systemctl start kubelet
+sudo systemctl enable kubelet.service
